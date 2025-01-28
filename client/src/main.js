4a3779603c90ade12989c8d25e4981057ec20b34
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-28 15:09:14 +0000

    render project

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,18 @@
 const app = document.getElementById("app");
+const API_URL = "https://webservice-1-fiqk.onrender.com";
 
 //fetch from the server
 
 async function getGoodGames() {
-  const baseUrl = "https://webservice-1-fiqk.onrender.com";
-  const response = await fetch(`${baseUrl}/games`);
-
-  const data = response.json();
+  const response = await fetch("http://localhost:8080/goodGames");
+  const data = await response.json();
   console.log(data);
+
+  data.forEach((game) => {
+    const p = document.createElement("p");
+    p.textContent = `${game.name}`;
+    app.appendChild(p);
+  });
 }
 
 getGoodGames();
--- server/server.js
+++ server/server.js
@@ -4,15 +4,14 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-let goodGames = [
-  { name: "Red Dead Redemption 2", platform: "PS4" },
-  { name: "Escape From Tarkov", platform: "PC" },
-  { name: "Halo 4", platform: "Xbox" },
-  { name: "Animal Crossing", platform: "Nintendo Switch" },
-];
+app.listen(8080, () => console.log("Listening on port 8080!"));
 
-app.get("/goodGames", (req, res) => {
+app.get("/goodGames", function (req, res) {
+  let goodGames = [
+    { name: "Red Dead Redemption 2", platform: "PS4" },
+    { name: "Escape From Tarkov", platform: "PC" },
+    { name: "Halo 4", platform: "Xbox" },
+    { name: "Animal Crossing", platform: "Nintendo Switch" },
+  ];
   res.json(goodGames);
 });
-
-app.listen(8080, () => console.log("Listening on port 8080!"));
