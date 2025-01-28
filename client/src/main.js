75d2a44f4de5bd23c22163bafd8b70e753035dbf
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-28 14:28:53 +0000

    render project

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ async function getGoodGames() {
   const response = await fetch(`${baseUrl}/games`);
 
   const data = response.json();
-  console.log(response.json());
+  console.log(data);
 }
 
 getGoodGames();
